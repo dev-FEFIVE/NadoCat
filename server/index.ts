a0cad360ba53b6cbbb31b99ee2d3cd7ad80c8228
ssoferret <owosso1212@gmail.com>
2024-07-30 23:21:48 +0900

    Feat: 타입스크립트 환경을 위한 데이터베이스 연결 모듈 작성

---
+++ server/index.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express';
+import connection from './mariadb';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const getTestString = (req: Request, res: Response) => {
+  const sql = 'SELECT * FROM TestString';
+  
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json(err);
+    } else {
+      console.log(results);
+      res.json(results);
+    }
+  });
+};
+
+const testApp = () => {
+  const router = express.Router();
+  router.use(express.json());
+  router.get('', getTestString);
+
+  const app = express();
+  app.use('', router);
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+};
+
+testApp();
