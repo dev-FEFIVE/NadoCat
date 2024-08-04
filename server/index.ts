c8b0756af2045f632555cbfc97b10e250016a83a
dogfootdragon <dogfootdragon@gmail.com>
2024-08-05 03:33:13 +0900

    Feat: 동네 고양이 도감 API 틀 생성, testString 테이블 레코드 반환 쿼리 테스트

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from "express";
 import CommunitiesRouter from "./routes/communities";
+import StreetCatsRouter from "./routes/streetCats";
 
 const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+console.log(PORT);
 
 const app = express();
 app.use(express.json());
 
 app.use("/boards/communities", CommunitiesRouter);
+app.use("/boards/street-cats", StreetCatsRouter);
 
 app.use((_req: Request, res: Response) => {
   res.sendStatus(404);
