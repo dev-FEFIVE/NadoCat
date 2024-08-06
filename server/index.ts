04e6332602c5c2cf6caaac2154df57fddcdcd447
Minep <pmh@ge.kornu.ac.kr>
2024-08-07 02:19:19 +0900

    Refactor: index.ts 수정

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import CommunitiesRouter from "./routes/communities";
 import StreetCatsRouter from "./routes/streetCats";
+import UserRouter from "./routes/users";
 
 const PORT = process.env.PORT || 3000;
 // const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/boards/communities", CommunitiesRouter);
 app.use("/boards/street-cats", StreetCatsRouter);
+app.use("/users", UserRouter);
 
 app.use((_req: Request, res: Response) => {
   res.sendStatus(404);
