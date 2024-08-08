7986ddf0220c26682f2c8548a6742ef6ee911d34
ssoferret <owosso1212@gmail.com>
2024-08-08 17:22:21 +0900

    Feat: 실종고양이 게시글 개별 조회 구현

---
+++ server/controller/missing/Missing.ts
@@ -0,0 +1,18 @@
+import prisma from "../../client";
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+export const getMissing = async (req: Request, res: Response) => {
+  try {
+    const results = await prisma.missings.findMany({
+      include: {
+        boardCategories: true,
+        users: true,
+        locations: true,
+      }
+    });
+    res.json(results);
+  } catch (error) {
+    res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
