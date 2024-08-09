c871cb104ae648daaedd9876da126b59a2043c94
ssoferret <owosso1212@gmail.com>
2024-08-09 12:48:05 +0900

    Rename: 폴더-파일명 컨벤션 변경 사항 반영

--- server/controller/missing/MissingsFavorites.ts
+++ server/controller/missing/MissingsFavorites.ts
@@ -1,33 +1,6 @@
-import prisma from "../client";
+import prisma from "../../client";
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-
-
-/**
- * TODO
- * 실종 고양이 전체 조회 게시판 
- * - [x] 전체 조회 API
- * - [x] 즐겨찾기 조회 API
- * - [x] 즐겨찾기 추가 API
- * - [x] 즐겨찾기 삭제 API
- */
-
-const include = {
-  board_categories: true,
-  users: true,
-  locations: true,
-};
-
-export const getMissings = async (req: Request, res: Response) => {
-  try {
-    const results = await prisma.missings.findMany({
-      include: include,
-    });
-    res.json(results);
-  } catch (error) {
-    res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
-  }
-};
 
 /**
  * CHECKLIST
@@ -48,7 +21,11 @@ export const getMissingFavorites = async (req: Request, res: Response) => {
           in: favoriteId
         }
       },
-      include: include,
+      include: {
+        boardCategories: true,
+        users: true,
+        locations: true,
+      }
     })
     res.json(results);
   } catch (error) {
