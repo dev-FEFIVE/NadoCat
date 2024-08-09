0f523fe36e989d787b7b35e82d45c73dcaefe5d3
ssoferret <owosso1212@gmail.com>
2024-08-09 17:31:26 +0900

    Feat: 실종 고양이 게시글 추가 API 라우터에 추가

--- server/routes/missings.ts
+++ server/routes/missings.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { getMissingFavorites, postMissingFavorites, deleteMissingFavorites } from "../controller/missing/MissingsFavorites";
 import { getMissings } from "../controller/missing/Missings";
+import { createMissing } from "../controller/missing/Missing";
 
 const router = express.Router();
 router.use(express.json());
 
 router.get("", getMissings);
+router.post("", createMissing);
 router.get("/favorites", getMissingFavorites);
 router.post("/favorites", postMissingFavorites);
 router.delete("/favorites/:postId", deleteMissingFavorites);
