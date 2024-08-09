f618425554bf6277ca56ef5a7a07ccdce8d13d98
ssoferret <owosso1212@gmail.com>
2024-08-09 18:42:22 +0900

    Feat: 실종 고양이 게시글 삭제 함수 라우터에 추가

--- server/routes/missings.ts
+++ server/routes/missings.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { getMissingFavorites, postMissingFavorites, deleteMissingFavorites } from "../controller/missing/MissingsFavorites";
 import { getMissings } from "../controller/missing/Missings";
-import { createMissing } from "../controller/missing/Missing";
+import { createMissing, deleteMissing } from "../controller/missing/Missing";
 
 const router = express.Router();
 router.use(express.json());
 
 router.get("", getMissings);
 router.post("", createMissing);
+router.delete("/:postId", deleteMissing);
 router.get("/favorites", getMissingFavorites);
 router.post("/favorites", postMissingFavorites);
 router.delete("/favorites/:postId", deleteMissingFavorites);
